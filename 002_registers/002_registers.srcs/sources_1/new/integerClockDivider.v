f0526312a7966f48a5a771cedb85c9b74022343e
Michael Ly <71558719+Michael-Q-Ly@users.noreply.github.com>
2022-02-15 05:44:07 +0000

    002_registers: Add integerClockDivider module

--- 002_registers/002_registers.srcs/sources_1/new/integerClockDivider.v
+++ 002_registers/002_registers.srcs/sources_1/new/integerClockDivider.v
@@ -19,8 +19,23 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
+`default_nettype	none
+module integerClockDivider( i_clk, o_led ) ;
+	parameter CLOCK_RATE_HZ = 100_000_000 ;					// Basys 3 default clock pin is 100 MHz
 
-module integerClockDivider(
+	input wire i_clk ;
+	output reg o_led ;
 
-    );
+	reg [26:0] counter ;
+	initial counter = 0 ;
+
+	always @( posedge i_clk ) begin
+		if ( counter >= CLOCK_RATE_HZ/2 - 1 ) begin
+			counter <= 0 ;
+			o_led <= !o_led ;
+		end
+		else begin
+			counter <= counter + 1 ;
+		end
+	end
 endmodule
